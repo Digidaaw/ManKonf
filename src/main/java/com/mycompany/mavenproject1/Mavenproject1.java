1bec41f86e57a3c77345e4f33b4671563c423862
Digidaaw <unknown151812@gmail.com>
2026-05-05 09:47:35 +0700

    Week 10 Manajemen Konfigurasi

---
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author ashar
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        Shouter shouter = new Shouter();
+        
+        shouter.shout("Hidup Mulyono");
+    }
+}
---
+++ src/main/java/com/mycompany/mavenproject1/Shouter.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author ashar
+ */
+public class Shouter {
+    public void shout (String message){
+        String shoutMessage = message.toUpperCase() + '!';
+        
+        System.out.println(shoutMessage);
+    }
+}
